43940f8d005c894da9053461ca88a2de1613a2a3
emilioarrig <earriaga@anahuac.mx>
2025-03-31 23:50:23 -0600

    Archivo de conexion a la db

--- conf/connection.php
+++ conf/connection.php
@@ -1,16 +1,21 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-$user = "root";
-$password="";
-$db = "sito-taxis";
-$host = "localhost";
+$user = "eguzman";
+$password = "contrasena123";
+$db = "reportes";
+$host = "127.0.0.1";
+$port = "5432";
 
-$sql_connect = "host=localhost port=5432 dbname=$db user=$user password=$password";
-$conn = pg_connect($sql_connect);
+$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$password");
 
-if(!$conn){
-    echo "Conexion fallida: ";
+if (!$conn) {
+    echo "❌ Conexión fallida: " . pg_last_error();
+} else {
+    echo "✅ Conexión exitosa gg papa";
 }
+
 
 
 
